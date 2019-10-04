257098e12f9931b82dea7f686b9c6747fd27f85a
ZachCashion <zach.cashion@gmail.com>
2019-10-04 16:58:57 -0400

    Update

--- ZachCashion_Personal_Website/js/jsMath.js
+++ ZachCashion_Personal_Website/js/jsMath.js
@@ -1,9 +1,54 @@
-// Get User Data
-// With jQuery
+// Code for jsMath
 
 $("#jsMathButton").click(function(){
 
+  // Step 1: Get User data
+  var num1 = Number($("#jsMathInput1").val());
+  var num2 = Number($("#jsMathInput2").val());
+  var num3 = Number($("#jsMathInput3").val());
+  var num4 = Number($("#jsMathInput4").val());
+  var num5 = Number($("#jsMathInput5").val());
+
+  // Step 2: Make caculations
+  var sum = num1 + num2 + num2 + num3 + num4 + num5;
+  var product = num1 * num2 * num3 * num4 * num5;
+  var array = [num1, num2, num3, num4, num5];
+
+  function myArrayMax(x) {
+     return Math.max.apply(null, x);
+  }
+
+  function myArrayMin(x) {
+     return Math.min.apply(null, x);
+  }
+
+
+
+  // Step 3: Output the results
+  var outputSum = "The Sum of your numbers is " + sum;
+  var outputMean = "The Mean of your numbers is " + (sum/5);
+  var outputProduct = "The Product of your numbers is " + product;
+  var outputGreatest = "The Greatest of your numbers is " + myArrayMax(array);
+  var outputLeast = "The least of your numbers is " + myArrayMin(array);
+
+  $("#sumResults").text(outputSum);
+  $("#meanResults").text(outputMean);
+  $("#productResults").text(outputProduct);
+  $("#greatestResults").text(outputGreatest);
+  $("#leastResults").text(outputLeast);
 });
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Make caculations
@@ -19,13 +64,7 @@ function compute(array){
    console.log('Greatest : ' +myArrayMax(array));
    console.log('Least : ' +myArrayMin(array));
 
-   function myArrayMax(x) {
-      return Math.max.apply(null, x);
-   }
 
-   function myArrayMin(x) {
-      return Math.min.apply(null, x);
-   }
 
 }
 
