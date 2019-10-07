2889c4fab30ac9b4dd37ca2b9e4bea690577770d
ZachCashion <zach.cashion@gmail.com>
2019-10-07 01:48:52 -0400

    Update

---
+++ ZachCashion_Personal_Website/js/jsFactorial.js
@@ -0,0 +1,17 @@
+// Code for js Factorial
+$("#jsFactorialButton").click(function(){
+  // Step 1: Get User Data
+  var inputFactorial = Number($("#jsFactorialInput").val());
+
+  // Step 2: Make Caculations
+  var i = inputFactorial - 1;
+  var factorial = inputFactorial;
+  for (i; i >= 1 ; i--) {
+    factorial *= i;
+  }
+
+  // Step 3: Output Results
+  var outputFactorial = "The Factorial of your number is " + factorial;
+
+  $("#factorialResults").text(outputFactorial);
+});
