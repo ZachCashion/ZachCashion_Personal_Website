5aedf375075b974817a9a436fd812d8155bf0bb4
ZachCashion <zach.cashion@gmail.com>
2019-12-02 14:44:09 -0500

    Update

--- ZachCashion_Personal_Website/js/sendEmail.js
+++ ZachCashion_Personal_Website/js/sendEmail.js
@@ -1,9 +1,52 @@
-Email.send({
-    SecureToken: "36734f33-aa11-4866-9407-d1c4a839bb88",
-    To: 'zach.cashion@gmail.com',
-    From: "zach.cashion@gmail.com",
-    Subject: "Portfolio Contact Form: " + name,
-    Body: message + name + email + phone
-}).then(
-    message => alert(message)
-);
+$(function () {
+
+    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function ($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function ($form, event) {
+            event.preventDefault(); // prevent default submit behaviour
+            // get values from FORM
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var phone = $("input#phone").val();
+            var message = $("textarea#message").val();
+            var firstName = name; // For Success/Failure Message
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
+            }
+            
+            Email.send({
+                SecureToken: "36734f33-aa11-4866-9407-d1c4a839bb88",
+                To: 'zach.cashion@gmail.com',
+                From: "zach.cashion@gmail.com",
+                Subject: "Portfolio Contact Form: " + name,
+                Body: "You have received a new message from your website contact form. " + "Here are the details: " + "Name: " + name + " Email: " + email + " Phone: " + phone + " Message: " + message
+            }).then(function () {
+                // Success message
+                $('#success').html("<div class='alert alert-success'>");
+                $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    .append("</button>");
+                $('#success > .alert-success')
+                    .append("<strong>Your message has been sent. </strong>");
+                $('#success > .alert-success')
+                    .append('</div>');
+                //clear all fields
+                $('#contactForm').trigger("reset");
+            })
+ 
+        },
+        filter: function () {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function (e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+// 
